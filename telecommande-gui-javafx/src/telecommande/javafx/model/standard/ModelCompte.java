8db2a1bbb19125a2c2e94e2e523eef8498e32271
plensy <leonelelanga@yahoo.fr>
2018-04-01 22:29:17 +0200

    controles de sécurite de compte et marque

--- telecommande-gui-javafx/src/telecommande/javafx/model/standard/ModelCompte.java
+++ telecommande-gui-javafx/src/telecommande/javafx/model/standard/ModelCompte.java
@@ -1,6 +1,5 @@
 package telecommande.javafx.model.standard;
 
-
 import static telecommande.javafx.model.EnumModeVue.CREER;
 import static telecommande.javafx.model.EnumModeVue.MODIFIER;
 
@@ -17,32 +16,26 @@ import telecommande.javafx.data.mapper.IMapper;
 import telecommande.javafx.model.EnumModeVue;
 import telecommande.javafx.model.IModelCompte;
 
-
 public class ModelCompte implements IModelCompte {
-	
-	
-	// Données observables 
-	
-	private final ObservableList<Compte> comptes = FXCollections.observableArrayList( 
-			c ->  new Observable[] { c.pseudoProperty()  } 
-		);
-	
-	private final Compte	compteVue = new Compte();
-	
-	
+
+	// Données observables
+
+	private final ObservableList<Compte> comptes = FXCollections
+			.observableArrayList(c -> new Observable[] { c.pseudoProperty() });
+
+	private final Compte compteVue = new Compte();
+
 	// Objet courant
 
-	private Compte			compteCourant;
-    private EnumModeVue		modeVue;
-	
-	
+	private Compte compteCourant;
+	private EnumModeVue modeVue;
+
 	// Autres champs
-	private IMapper			mapper;
-	private IServiceCompte	serviceCompte;
-	
-	
+	private IMapper mapper;
+	private IServiceCompte serviceCompte;
+
 	// Getters
-	
+
 	@Override
 	public ObservableList<Compte> getComptes() {
 		return comptes;
@@ -52,102 +45,108 @@ public class ModelCompte implements IModelCompte {
 	public Compte getCompteVue() {
 		return compteVue;
 	}
-	
-	
+
 	// Injecteurs
-	
+
 	public void setMapper(IMapper mapper) {
 		this.mapper = mapper;
 	}
-	
+
 	public void setServiceCompte(IServiceCompte serviceCompte) {
 		this.serviceCompte = serviceCompte;
 	}
-	
-	
+
 	// Actualisations
-	
+
 	@Override
 	public void actualiserListe() {
-		
+
 		// Prépare la récupération de l'objet courant
 		int idCourant = 0;
-		if( compteCourant != null ) {
+		if (compteCourant != null) {
 			idCourant = compteCourant.getId();
 		}
-		
+
 		// Actualise la liste des comptes
 		comptes.clear();
-		for( DtoCompte dto : serviceCompte.listerTout() ) {
-			Compte compte = mapper.map( dto );
-			comptes.add( compte );
-			if( compte.getId() == idCourant ) {
+		for (DtoCompte dto : serviceCompte.listerTout()) {
+			Compte compte = mapper.map(dto);
+			comptes.add(compte);
+			if (compte.getId() == idCourant) {
 				compteCourant = compte;
 			}
 		}
- 	}
-	
-	
+	}
+
 	// Actions
-	
+
 	@Override
 	public void preparerAjouter() {
-        modeVue = CREER;
-		mapper.update( new Compte(), compteVue );		
+		modeVue = CREER;
+		mapper.update(new Compte(comptes.get(comptes.size() - 1).getId() + 1, "", "", "", "", ""), compteVue);
 	}
-	
+
 	@Override
-	public void preparerModifier( Compte compte ) {
-        modeVue = MODIFIER;
+	public void preparerModifier(Compte compte) {
+		modeVue = MODIFIER;
 		compteCourant = compte;
-		mapper.update( compte, compteVue );	
+		mapper.update(compte, compteVue);
 	}
-	
-	
+
 	@Override
-	public void validerMiseAJour() throws ExceptionValidation  {
+	public void validerMiseAJour() throws ExceptionValidation {
 
 		// Crée un objet contenant le données pour la mise à jour
-		DtoCompte dto = mapper.map( compteVue );
-		
-		// Effectue la mise à jour
-        if ( modeVue == CREER ) {
-			int id = serviceCompte.inserer(dto);
-			compteVue.setId(id);
-            compteCourant = mapper.update(compteVue, new Compte());
-			comptes.add( compteCourant );
-		}
-        if ( modeVue == MODIFIER ) {
-			serviceCompte.modifier(dto);
-			mapper.update( compteVue, compteCourant );		
-		}
+		DtoCompte dto = mapper.map(compteVue);
 
-        // Trie la liste
-        trierListe();
+		// Effectue la mise à jour
+		if (compteVue.getNom().equals("")) {
+			throw new ExceptionValidation("Le champ Nom ne doit pas être vide");
+		} else if (compteVue.getEmail().equals("")) {
+			throw new ExceptionValidation("Le champ Mail ne doit pas être vide");
+		} else if (compteVue.getMotDePasse().equals("")) {
+			throw new ExceptionValidation("Le champ Mot de passe ne doit pas être vide");
+		} else if (compteVue.getPrenom().equals("")) {
+			throw new ExceptionValidation("Le champ Prenom ne doit pas être vide");
+		} else if (compteVue.getPseudo().equals("")) {
+			throw new ExceptionValidation("Le champ Pseudo ne doit pas être vide");
+		} else if (compteVue.getRoles().isEmpty()) {
+			throw new ExceptionValidation("Choisir le rôle ");
+		} else {
+			if (modeVue == CREER) {
+				int id = serviceCompte.inserer(dto);
+				compteVue.setId(id);
+				compteCourant = mapper.update(compteVue, new Compte());
+				comptes.add(compteCourant);
+			}
+			if (modeVue == MODIFIER) {
+				serviceCompte.modifier(dto);
+				mapper.update(compteVue, compteCourant);
+			}
+
+			// Trie la liste
+			trierListe();
+		}
 	}
-	
-	
+
 	@Override
-	public void supprimer( Compte compte ) throws ExceptionValidation  {
-		serviceCompte.supprimer( compte.getId() );
-		comptes.remove( compte );
+	public void supprimer(Compte compte) throws ExceptionValidation {
+		serviceCompte.supprimer(compte.getId());
+		comptes.remove(compte);
 	}
-	
-	
+
 	// Initialisaiton
-	
+
 	public void refresh() {
 		actualiserListe();
 	}
-    
-    
-    // Méthodes auxiliaires
-    
-    private void trierListe() {
-		FXCollections.sort( comptes,
-            (Comparator<Compte>) ( c1, c2) -> {
-                return ( c1.pseudoProperty().get().toUpperCase().compareTo(c2.pseudoProperty().get().toUpperCase()));
+
+	// Méthodes auxiliaires
+
+	private void trierListe() {
+		FXCollections.sort(comptes, (Comparator<Compte>) (c1, c2) -> {
+			return (c1.pseudoProperty().get().toUpperCase().compareTo(c2.pseudoProperty().get().toUpperCase()));
 		});
-    }
+	}
 
 }
--- telecommande-gui-javafx/src/telecommande/javafx/model/standard/ModelMarque.java
+++ telecommande-gui-javafx/src/telecommande/javafx/model/standard/ModelMarque.java
@@ -1,6 +1,5 @@
 package telecommande.javafx.model.standard;
 
-
 import static telecommande.javafx.model.EnumModeVue.CREER;
 import static telecommande.javafx.model.EnumModeVue.MODIFIER;
 
@@ -17,32 +16,26 @@ import telecommande.javafx.data.mapper.IMapper;
 import telecommande.javafx.model.EnumModeVue;
 import telecommande.javafx.model.IModelMarque;
 
-
 public class ModelMarque implements IModelMarque {
-	
-	
-	// Données observables 
-	
-	private final ObservableList<Marque> marques = FXCollections.observableArrayList( 
-			c ->  new Observable[] { c.nomProperty()  } 
-		);
-	
-	private final Marque	marqueVue = new Marque();
-	
-	
+
+	// Données observables
+
+	private final ObservableList<Marque> marques = FXCollections
+			.observableArrayList(c -> new Observable[] { c.nomProperty() });
+
+	private final Marque marqueVue = new Marque();
+
 	// Objet courant
 
-	private Marque			marqueCourant;
-    private EnumModeVue		modeVue;
-	
-	
+	private Marque marqueCourant;
+	private EnumModeVue modeVue;
+
 	// Autres champs
-	private IMapper			mapper;
-	private IServiceMarque	serviceMarque;
-	
-	
+	private IMapper mapper;
+	private IServiceMarque serviceMarque;
+
 	// Getters
-	
+
 	@Override
 	public ObservableList<Marque> getMarques() {
 		return marques;
@@ -52,102 +45,98 @@ public class ModelMarque implements IModelMarque {
 	public Marque getMarqueVue() {
 		return marqueVue;
 	}
-	
-	
+
 	// Injecteurs
-	
+
 	public void setMapper(IMapper mapper) {
 		this.mapper = mapper;
 	}
-	
+
 	public void setServiceMarque(IServiceMarque serviceMarque) {
 		this.serviceMarque = serviceMarque;
 	}
-	
-	
+
 	// Actualisations
-	
+
 	@Override
 	public void actualiserListe() {
-		
+
 		// Prépare la récupération de l'objet courant
 		int idCourant = 0;
-		if( marqueCourant != null ) {
+		if (marqueCourant != null) {
 			idCourant = marqueCourant.getIdMarque();
 		}
-		
+
 		// Actualise la liste des marques
 		marques.clear();
-		for( DtoMarque dto : serviceMarque.listerTout() ) {
-			Marque marque = mapper.map( dto );
-			marques.add( marque );
-			if( marque.getIdMarque() == idCourant ) {
+		for (DtoMarque dto : serviceMarque.listerTout()) {
+			Marque marque = mapper.map(dto);
+			marques.add(marque);
+			if (marque.getIdMarque() == idCourant) {
 				marqueCourant = marque;
 			}
 		}
- 	}
-	
-	
+	}
+
 	// Actions
-	
+
 	@Override
 	public void preparerAjouter() {
-        modeVue = CREER;
-		mapper.update( new Marque(), marqueVue );		
+		modeVue = CREER;
+		mapper.update(new Marque(marques.get(marques.size()-1).getIdMarque()+1,""), marqueVue);
 	}
-	
+
 	@Override
-	public void preparerModifier( Marque marque ) {
-        modeVue = MODIFIER;
+	public void preparerModifier(Marque marque) {
+		modeVue = MODIFIER;
 		marqueCourant = marque;
-		mapper.update( marque, marqueVue );	
+		mapper.update(marque, marqueVue);
 	}
-	
-	
+
 	@Override
-	public void validerMiseAJour() throws ExceptionValidation  {
+	public void validerMiseAJour() throws ExceptionValidation {
 
 		// Crée un objet contenant le données pour la mise à jour
-		DtoMarque dto = mapper.map( marqueVue );
-		
-		// Effectue la mise à jour
-        if ( modeVue == CREER ) {
-			int id = serviceMarque.inserer(dto);
-			marqueVue.setIdMarque(id);
-            marqueCourant = mapper.update(marqueVue, new Marque());
-			marques.add( marqueCourant );
-		}
-        if ( modeVue == MODIFIER ) {
-			serviceMarque.modifier(dto);
-			mapper.update( marqueVue, marqueCourant );		
-		}
+		DtoMarque dto = mapper.map(marqueVue);
 
-        // Trie la liste
-        trierListe();
+		// Effectue la mise à jour
+		if (marqueVue.getNom().equals("")) {
+			throw new ExceptionValidation("Le champ Nom ne doit pas être vide");
+		} else {
+			if (modeVue == CREER) {
+				int id = serviceMarque.inserer(dto);
+				marqueVue.setIdMarque(id);
+				marqueCourant = mapper.update(marqueVue, new Marque());
+				marques.add(marqueCourant);
+			}
+			if (modeVue == MODIFIER) {
+				serviceMarque.modifier(dto);
+				mapper.update(marqueVue, marqueCourant);
+			}
+
+			// Trie la liste
+			trierListe();
+		}
 	}
-	
-	
+
 	@Override
-	public void supprimer( Marque marque ) throws ExceptionValidation  {
-		serviceMarque.supprimer( marque.getIdMarque() );
-		marques.remove( marque );
+	public void supprimer(Marque marque) throws ExceptionValidation {
+		serviceMarque.supprimer(marque.getIdMarque());
+		marques.remove(marque);
 	}
-	
-	
+
 	// Initialisaiton
-	
+
 	public void refresh() {
 		actualiserListe();
 	}
-    
-    
-    // Méthodes auxiliaires
-    
-    private void trierListe() {
-		FXCollections.sort( marques,
-            (Comparator<Marque>) ( c1, c2) -> {
-                return ( c1.nomProperty().get().toUpperCase().compareTo(c2.nomProperty().get().toUpperCase()));
+
+	// Méthodes auxiliaires
+
+	private void trierListe() {
+		FXCollections.sort(marques, (Comparator<Marque>) (c1, c2) -> {
+			return (c1.nomProperty().get().toUpperCase().compareTo(c2.nomProperty().get().toUpperCase()));
 		});
-    }
+	}
 
 }
